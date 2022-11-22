af5519219354938a2d1ab3f564e29c7f3bc0a93e
Dmitriy <zdv9997@mail.ru>
2022-11-22 10:26:52 +0200

    находим расстояние между точками в 3D

---
+++ zad21/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите координаты точки А (x, y, z):");
+int[] point_a = new int[3];
+int[] point_b = new int[3];
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Коородината : ");
+    point_a[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введите координаты точки B (x, y, z):");
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Коородината : ");
+    point_b[i] = Convert.ToInt32(Console.ReadLine());
+}
+double dist = 0;
+for (int i = 0; i < 3; i++) dist = dist + Math.Pow(point_a[i] - point_b[i], 2);
+dist = Math.Round(Math.Sqrt(dist), 2);
+Console.WriteLine($"Расстояние между точками равно: {dist}");
+
