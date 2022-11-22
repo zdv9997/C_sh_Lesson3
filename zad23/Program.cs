ffbeffe5430b47a7795e8d70996a4377e09faea3
Dmitriy <zdv9997@mail.ru>
2022-11-22 10:34:39 +0200

    выводим таблицу кубов вводимого числа

---
+++ zad23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Куб числа {i} равен: {Math.Pow(i, 3)}");
+}
